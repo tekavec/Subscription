86a826c5b7480ebe1390a5706487f9ea9ac577de
tekavec <tomaz.tekavec@gmail.com>
2019-11-10 20:01:46 +0000

    Add unit tests project

---
+++ Subscription.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Subscription.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
